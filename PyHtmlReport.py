ac0c1e47d77e16794bfd7d30a53a3c881ac9a897
martin_delagorce <martin_delagorce@localhost>
2018-04-03 21:09:40 +0200

    improving figure export to plotly

--- PyHtmlReport.py
+++ PyHtmlReport.py
@@ -2,15 +2,21 @@ import matplotlib.pyplot as plt, mpld3
 from scipy.misc import imsave
 import os
 import hashlib
-
-
+import json
+import numpy as np
+# http://visjs.org/index.html#download_install
+# https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.min.js
+#https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.min.css
 class htmlReport():
     def __init__(self,defaultImageDirectory='.'):
         self.bodyList=[]
-        if not os.path.isdir(defaultImageDirectory):
-            os.makedirs(defaultImageDirectory)
-	
+        self.scriptlist=[]
+        self.stylelist=[]  
         self.defaultImageDirectory=defaultImageDirectory
+        self.file=None
+        self.usePLotly=False
+        if not os.path.isdir(defaultImageDirectory):
+            os.makedirs(defaultImageDirectory)        
     
     def write(self,string,newline=True):
         self.bodyList.append(string)
@@ -34,13 +40,132 @@ class htmlReport():
         self.addFig(fig)
         plt.close(fig)
 
-    def addFig(self,fig):
-        self.write(mpld3.fig_to_html(fig))
+    def addFig(self,fig,method='mpld3',width=None,height=None):
+        fig.canvas.draw_idle()
+        plt.show()
+        
+        ax=fig.gca()
+        title=ax.get_title()
+        if title=='':
+            print ('you should add a title to the figure')
+        if method=='mpld3':
+            if  hasattr(fig.gca(), 'get_zlim'):
+                print ('3D plot are not handled by mpld3 yet , use plotly instead')
+                raise
+            self.write(mpld3.fig_to_html(fig))
+        if method=='visjs':
+            # mpld3 does not seem to support 3D plots
+            # could use http://visjs.org/graph3d_examples.html   
+            pass
+        if method=='plotly':
+            
+            figsize = fig.get_size_inches()*fig.dpi # size in pixels            
+            if width is None:
+                width=figsize[0]
+            if height is None:
+                width=figsize[1]                
+            jsonLayout,jsonData=self.matplotlibToPlotlyJson(ax)
+            str='<div class="plotly"' ;
+            if (not width is None) or (not height is None):
+                str+='style="';
+                if not width is None:
+                    str+='width:%dpx;'%width
+                if not height is None:
+                    str+='height:%dpx;'%height
+                str+='"'
+            str+="data='%s' layout='%s' ></div>"%(jsonData,jsonLayout)
+            self.write(str)  
+            self.usePLotly=True
+            
+        if method=='svg':
+            pass
+            
+        
+    def matplotlibToPlotlyJson(self,ax):
+ 
+        linesPlotly=[];
+        is3D=hasattr(ax, 'get_zlim')
+        hasline=False
+        for line in ax.get_lines():
+            s={}
+            hasline=True
+            if is3D:
+               
+                s['x']=line._verts3d[0].tolist()
+                s['y']=line._verts3d[1].tolist()
+                s['z']=line._verts3d[2].tolist()
+                s['type']= 'scatter3d'
+                s['mode']= 'lines'
+            else:
+                s['x']=line.get_xdata().tolist()
+                s['y']=line.get_ydata().tolist()                
+                s['name']=line.get_label()
+                s['legendgroup']=line.get_label()
+                s['showlegend']=True
+                s['line']={'color':color[0:3],'width':line.get_lw()}
+            color=line.get_color()
+            
+            linesPlotly.append(s)
+            
+            
+        for collection in ax.collections:
+            s={}
+            if  is3D:
+                tmp=collection.get_offsets()
+                
+                #s['x']=tmp[:,0].tolist()
+                #s['y']=tmp[:,1].tolist()  
+                if np.any(np.isnan(tmp)):
+                    raise ('nans not handled yet for plotly export')
+                tmp=collection._offsets3d
+                s['x']=list(tmp[0])
+                s['y']=list(tmp[1])
+                s['z']=list(tmp[2])
+                s['type']= 'scatter3d'
+                
+              
+            else:
+                tmp=collection.get_offsets()
+                
+                s['x']=tmp[:,0].tolist()
+                s['y']=tmp[:,1].tolist()   
+                s['type']= 'scatter'
+                
+            
+     
+            s['mode']=  'markers'
+           
+     
+            makersDict={'+':'cross','d':'diamond','x':'x-dot','s':'square'}
+            markerSize=collection.get_sizes()/10
+            s['marker']={'symbol':'circle','size':int(markerSize[0]),'sizemode':'diameter'}# not sure how to retrieve the mqrker shape in matplotlib
+            s['name']=collection.get_label()
+            s['legendgroup']=collection.get_label()
+            s['showlegend']=True 
+            linesPlotly.append(s)
+            
+        layout={}
+        layout['title']=ax.get_title()
+      
+
+        if is3D:
+            layout['scene']={}
+            plt.pause(1)# the axis infor,mation seem to lag 
+            layout['scene']['xaxis']={'tile':ax.get_xlabel(),'range':ax.get_xbound()}
+            layout['scene']['yaxis']={'tile':ax.get_ylabel(),'range':ax.get_ybound()}            
+            layout['scene']['zaxis']={'tile':ax.get_zlabel(),'range':ax.get_zbound()}
+        else:
+            layout['xaxis']={'tile':ax.get_xlabel(),'range':ax.get_xbound()}
+            layout['yaxis']={'tile':ax.get_ylabel(),'range':ax.get_ybound()}            
+        jsonLayout=json.dumps(layout)
+        jsonData=json.dumps(linesPlotly)
+        return jsonLayout,jsonData
+    
         
     def setDefaultImageDirectory(self,directory):
         self.defaultImageDirectory=directory
         
-    def saveAndIncludeGraphic(self,im,name=None,width=None,height=None):
+    def addImage(self,im,name=None,width=None,height=None):
         if name is None:
             name=hashlib.sha1(im).hexdigest()   
             filename=name+'.png'
@@ -63,16 +188,33 @@ class htmlReport():
             self.bodyList.append(' height="%d"'%height)
         self.bodyList.append('>')
     
+    def addJavascriptSource():
+        pass
     
+
+   
     
     def save(self,file=None):
         if file is None:
             file=os.path.join(self.defaultImageDirectory,'report.html')
         output= open(file, 'w') 
-        output.write('<!DOCTYPE html><html><body>')
+        output.write('<!DOCTYPE html><html><body>\n')
+        output.write('<head>\n');
+        if self.usePLotly:
+            output.write(' <script src="https://code.jquery.com/jquery-1.12.4.js"></script>\n')
+            output.write('<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>\n');
+        output.write('</head>\n');        
+
+        output.write('<body>\n' )
         for bodyElt in self.bodyList : 
             output.write(bodyElt)
-        output.write('</body></html>' )
+        output.write('</body></html>\n' )
+        
+        if self.usePLotly:
+            output.write('  <script>$( ".plotly" ).each(function( index ) {Plotly.plot( this ,jQuery.parseJSON($( this ).attr( "data")),jQuery.parseJSON($( this ).attr( "layout")));});</script>')
+            output.write('  <script>(function() { $(''.zoomablesvg'').each(function( index ){var $panzoom = $(this).panzoom(); $panzoom.on(''mousewheel.focal'', function( e ) {var delta = e.delta || e.originalEvent.wheelDelta; var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0; $panzoom.panzoom(''zoom'', zoomOut, { increment: 0.1, animate: false,  focal: e    });   }  )  }); })();</script>')
+        
+        output.write('   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.21.0/vis.min.js"></script>   ') 
         output.close()
         
  
--- examples.py
+++ examples.py
@@ -3,39 +3,53 @@ from PyHtmlReport import htmlReport
 import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-        
-   
-def example1():
-	
-	Im=np.random.rand(30,60)
- 
-	plt.ion()
-        mpl.rcParams['legend.fontsize'] = 10
-        
-        fig = plt.figure(figsize=[3,3])
-        #ax = fig.gca(projection='3d')# does not seem to work with mpld3
-	ax = fig.gca()
-        theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-        z = np.linspace(-2, 2, 100)
-        r = z**2 + 1
-        x = r * np.sin(theta)
-        y = r * np.cos(theta)
-        ax.plot(x, y, label='parametric curve')
-	#ax.plot(x.tolist(), y.tolist(), label='parametric curve')
-        ax.legend()
-	#mpld3.show()
-	#plt.show()
-        report=htmlReport()
-	
+from mpl_toolkits.mplot3d import art3d
 
-        #report.addPlot(fig)
-	report.saveAndIncludeGraphic(Im,width=300)
-	report.newlines(5)
-	report.saveAndIncludeGraphic(Im,width=300)
-        report.save('test3D.html')
-        
+def example1():
+
+    Im=np.random.rand(30,60)
+
+    plt.ion()
+    mpl.rcParams['legend.fontsize'] = 10
+
+    fig = plt.figure(figsize=[3,3])
+    #ax = fig.gca(projection='3d')# does not seem to work with mpld3
+    ax = fig.gca()
+    theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+    z = np.linspace(-2, 2, 100)
+    r = z**2 + 1
+    x = r * np.sin(theta)
+    y = r * np.cos(theta)
+    ax.plot(x, y, label='parametric curve')
+    
+    cloud=np.random.rand(60,2)*5
+    
+    ax.scatter(cloud[:,0],cloud[:,1])
+    
+    ax.set_title('simple plot example')
+    ax.legend()
+    #mpld3.show()
+    #plt.show()
+    
+    cloud=np.random.rand(60,3)
+    fig3D = plt.figure()
+    ax = fig3D.add_subplot(111, projection='3d')    
+    ax.scatter(cloud[:,0],cloud[:,1],cloud[:,2],marker ='d',s=2)
+    plt.show()
+    
+    
+    report=htmlReport()
+
+
+    report.addFig(fig,method='plotly')
+    report.addFig(fig3D,method='plotly')
+    report.addImage(Im,width=300)
+    report.newlines(5)
+    report.addImage(Im,width=300)
+    report.save('test3D.html')
+
 
 if __name__ == "__main__":
-	example1()
-	
+    example1()
+
 
